818c5bbe57f9832f666923b48809fe8bfa786441
geeker254 <eugenebiwott@gmail.com>
2020-07-20 13:52:29 +0300

    custon form design

--- JS/index.js
+++ JS/index.js
@@ -119,3 +119,33 @@ if (day < 10) {
 
   return day + ' / ' + month + ' / ' + year;
 }
+
+
+var Gender = Gender1, Gender2;
+var Gender1 = male;
+var Gender2 = female
+
+//Array list male
+var male = new Object();
+
+male.Sunday = "Kwasi"
+male.Monday = "Kwadwo";
+male.Tuesday = "Kwabena";
+male.Wednesday = "Kwaku";
+male.Thursday = "Yaw";
+male.Friday = "Kofi";
+male.Saturday = "Kwame";
+
+
+
+//Array list female
+var female = new Object();
+
+female.Sunday = "Akosua";
+female.Monday = "Adwoa";
+female.Tuesday = "Abenaa";
+female.Wednesday = "Akua";
+female.Thursday = "Yaa";
+female.Friday = "Afua";
+female.Saturday = "Ama";
+
