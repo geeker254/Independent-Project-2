1e0ae1d0bb64d29368cb9346c0b16f04f6a3b597
geeker254 <eugenebiwott@gmail.com>
2020-07-20 08:55:42 +0300

    Datepickerkeypad

--- JS/index.js
+++ JS/index.js
@@ -1,46 +1,27 @@
-
-var Gender = Gender1, Gender2;
-var Gender1 = male;
-var Gender2 = female;
-//Array list male
-var male = new Object();
-
-male.Sunday = "Kwasi"
-male.Monday = "Kwadwo";
-male.Tuesday = "Kwabena";
-male.Wednesday = "Kwaku";
-male.Thursday = "Yaw";
-male.Friday = "Kofi";
-male.Saturday = "Kwame";
-
-//Array list female
-var female = new Object();
-
-female.Sunday = "Akosua";
-female.Monday = "Adwoa";
-female.Tuesday = "Abenaa";
-female.Wednesday = "Akua";
-female.Thursday = "Yaa";
-female.Friday = "Afua";
-female.Saturday = "Ama";
+  var Gender = Gender1, Gender2;
+  var Gender1 = male;
+  var Gender2 = female;
+  //Array list male
+  var male = new Object();
+  
+  male.Sunday = "Kwasi"
+  male.Monday = "Kwadwo";
+  male.Tuesday = "Kwabena";
+  male.Wednesday = "Kwaku";
+  male.Thursday = "Yaw";
+  male.Friday = "Kofi";
+  male.Saturday = "Kwame";
 
 
+  
+  //Array list female
+  var female = new Object();
+  
+  female.Sunday = "Akosua";
+  female.Monday = "Adwoa";
+  female.Tuesday = "Abenaa";
+  female.Wednesday = "Akua";
+  female.Thursday = "Yaa";
+  female.Friday = "Afua";
+  female.Saturday = "Ama";
 
-if ((validcustom03)(d) (d<=0) (d>31) ) {
-  alert = ("invalid feedback")
-}
-
-if ((validcustom02)(m) (m<= 0) (m > 12)) {
-  alert = ("valid feedback")  
-}
-
-
-var male = document.getElementById("validationCustom04");
-var female = document.getElementById("validationCustom04");
-
-if (Gender == Gender1) {
-  Int8Array();  
-}
-if (Gender == GENDER2) {
-  Int18Array();
-}
